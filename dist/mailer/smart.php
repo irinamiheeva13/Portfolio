111403acac92057553a9778219b8bc4655735d46
Irina Mikheeva <irinamiheeva13@gmail.com>
2021-03-18 20:00:59 +0300

    removed unused code

--- dist/mailer/smart.php
+++ dist/mailer/smart.php
@@ -13,13 +13,13 @@ $mail->CharSet = 'utf-8';
 $mail->isSMTP();                                      // Set mailer to use SMTP
 $mail->Host = 'smtp.gmail.com';  // Specify main and backup SMTP servers
 $mail->SMTPAuth = true;                               // Enable SMTP authentication
-$mail->Username = 'irinamikheeva.website@gmail.com';                 // Наш логин
-$mail->Password = 'Portfolio1311@';                           // Наш пароль от ящика
+$mail->Username = 'irinamiheeva13';                 // Наш логин
+$mail->Password = 'MoistPaper2309@';                           // Наш пароль от ящика
 $mail->SMTPSecure = 'ssl';                            // Enable TLS encryption, `ssl` also accepted
 $mail->Port = 465;                                    // TCP port to connect to
  
-$mail->setFrom('irinamikheeva.website@gmail.com', 'Portfolio');   // От кого письмо 
-$mail->addAddress('irinamiheeva13@gmail.com');     // Add a recipient
+$mail->setFrom('irinamiheeva13@gmail.com', 'Portfolio');   // От кого письмо 
+$mail->addAddress('');     // Add a recipient
 //$mail->addAddress('ellen@example.com');               // Name is optional
 //$mail->addReplyTo('info@example.com', 'Information');
 //$mail->addCC('cc@example.com');
